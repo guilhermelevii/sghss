8abbe1e0e0913c090dddb82326fadefd8a9bc930
guilhermelevii <guilhermelevii.gl@gmail.com>
2025-11-23 21:08:00 -0300

    feat: Study,Documention

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -1,8 +1,15 @@
 import { Component } from '@angular/core';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatCardModule } from '@angular/material/card';
+import { MatListModule } from '@angular/material/list';
 
 @Component({
   selector: 'app-dashboard',
-  imports: [],
+  imports: [MatSidenavModule,
+    MatToolbarModule,
+    MatCardModule,
+    MatListModule],
   templateUrl: './dashboard.html',
   styleUrl: './dashboard.css',
 })
